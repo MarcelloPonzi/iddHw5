b03176b35401286d79775bf9f282eb11863264fa
MarcelloPonzi <marcelloponzi@gmail.com>
2022-12-06 21:20:34 +0100

    compagnie usa e statistiche

--- SpiderSpy/spiders/wikiUsa_spider.py
+++ SpiderSpy/spiders/wikiUsa_spider.py
@@ -1,4 +1,8 @@
+import time
+
 import scrapy
+import scrapy.statscollectors
+from ..statistiche import Statistiche
 
 
 class WikiSpider(scrapy.Spider):
@@ -9,49 +13,72 @@ class WikiSpider(scrapy.Spider):
     custom_settings = {
         'DEPTH_LIMIT': 3
     }
+    stats = Statistiche()
 
     def parse(self, response, **kargs):
-        companies_pages = response.xpath("//div/ul/li/a")
+
+        f = open("link trovati.txt", "w")
+
+        """Prende i link alle pagine di compagnie direttamente sulla sorgente"""
+        companies_pages = response.xpath("//div[@class='mw-parser-output']/ul/li/a")
+        num_pagine = 0
         for company_page in companies_pages:
+            num_pagine = num_pagine + 1
+            self.stats.inc_request()
             yield response.follow(url=company_page, callback=self.parse_company_page)
 
-        states_links = response.xpath("//div[@role='note']/a")
-        for state_link in states_links:
-            yield response.follow(url=state_link, callback=self.parse_li_list())
-            yield response.follow(url=state_link, callback=self.parse_company_table)
 
+        """Prende i link alle liste di compagnie dei singoli stati"""
+        states_links = response.xpath("//div[@role='note']/a")
+        link_stati = 0
+        for state_link in states_links:
+            link_stati = link_stati + 1
+            self.stats.inc_request()
+            yield response.follow(url=state_link, callback=self.parse_li_list)
+            yield response.follow(url=state_link, callback=self.parse_company_table)
+        f.write("pagine compagnie: " + str(num_pagine) + "\n" + "link stati: " + str(link_stati))
+        f.close()
+
+    def parse_li_list(self, response):
+        companies_pages = response.xpath("//div[@class='mw-parser-output']/ul/li/a")
+        for company_page in companies_pages:
+            self.stats.inc_request()
+            yield response.follow(url=company_page, callback=self.parse_company_page)
 
     def parse_company_table(self, response):
         table = response.xpath("//table/caption/following-sibling::tbody/tr")
         for row in table:
-
             name = row.xpath(".//td[1]/a/text()").get()
             industry = row.xpath(".//td[2]/text()").get()
             headquarters = row.xpath(".//td[4]/a/text()").get()
             founded = row.xpath(".//td[5]/text()").get()
-
             if type(name) is str:
+                self.stats.hitted()
                 yield {
                     'Name': name,
                     'Industry': industry,
                     'Headquarters': headquarters,
                     'Founded': founded
                 }
+        self.stats.reset_hit()
+        self.stats.print_stats()
 
 
     def parse_company_page(self, response):
         info_box = response.xpath("//table[@class='infobox vcard']")
         name = info_box.xpath(".//text()").get()
-        industry = info_box.xpath(".//tbody/tr[3]/td/a/text()").get()
-        headquarters = info_box.xpath(".//tbody/tr[5]/td/a/text()").get()
-        founded = info_box.xpath(".//tbody/td[4]/text()").get()
+        industry = info_box.xpath(".//tbody/tr/th[text()='Industry']/following-sibling::td/text()").get()
+        headquarters = info_box.xpath(".//tbody/tr/th[text()='Headquarters']/following-sibling::td/a/text()").get()
+        founded = info_box.xpath(".//tbody/tr/th[text()='Founded']/following-sibling::td/text()").get()
 
         if type(name) is str:
+            self.stats.hitted()
             yield {
                 'Name': name,
                 'Industry': industry,
                 'Headquarters': headquarters,
                 'Founded': founded
             }
-
+        self.stats.reset_hit()
+        self.stats.print_stats()
 
--- SpiderSpy/statistiche.py
+++ SpiderSpy/statistiche.py
@@ -1,1 +1,33 @@
-class statistiche
+import math
+
+class Statistiche:
+
+    hit = False
+    hit_count = 0
+    request_count = 0
+
+    def __init__(self):
+        return
+
+    def inc_hit(self):
+        self.hit_count = self.hit_count+1
+
+    def inc_request(self):
+        self.request_count = self.request_count + 1
+
+    def reset_hit(self):
+        self.hit = False
+
+    def hitted(self):
+        self.hit = True
+        self.inc_hit()
+
+    def print_stats(self):
+        print("Numero di richieste: " + str(self.request_count))
+        print("Risposte contenenti informazioni: " + str(self.hit_count))
+        print("Accuratezza: " + str(math.trunc((self.hit_count/self.request_count)*100)) + "%")
+        f = open("statistiche.txt", "w")
+        f.write("Numero di richieste: " + str(self.request_count) + "\n" + "Risposte contenenti informazioni: " + str(self.hit_count) + "\n" + "Accuratezza: " + str(math.trunc((self.hit_count/self.request_count)*100)) + "%")
+        f.close()
+
+
